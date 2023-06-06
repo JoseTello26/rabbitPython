3a9d6ea3d38922a6b6c250ce2ca2ccae72be8293
JoseTello26 <39140021+JoseTello26@users.noreply.github.com>
2023-06-05 21:14:41 -0500

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,6 +1,6 @@
 import pika, json
 credentials = pika.PlainCredentials('guest', 'guest')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672, virtual_host='/', credentials=credentials, blocked_connection_timeout=60))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.17.0.2', port=5672, virtual_host='/', credentials=credentials, blocked_connection_timeout=60))
 channel = connection.channel()
 channel.queue_declare(queue='user')
 
