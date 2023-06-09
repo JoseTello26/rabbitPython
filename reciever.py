3c189ef9fd2a4f902a01d37b031f8006a2218368
JoseTello26 <39140021+JoseTello26@users.noreply.github.com>
2023-06-09 00:30:09 -0500

    Update reciever.py

--- reciever.py
+++ reciever.py
@@ -1,15 +1,16 @@
 import pika, sys, os, json, psycopg2
 login=False
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=os.environ('RABBITMQ_HOST'), port=os.environ('RABBITMQ_PORT')))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=os.environ['RABBITMQ_HOST'], port=os.environ['RABBITMQ_PORT']))
 channel = connection.channel()
 
-channel.queue_declare(queue='user')
+channel.queue_declare(queue='js-python')
+channel.queue_declare(queue='python-js')
 
 conn = psycopg2.connect(dbname="tienda",
-                        user=os.environ('POSTGRES_USER'),
-                        password=os.environ('POSTGRES_PASSWORD'),
-                        host=os.environ('POSTGRES_HOST'),
-                        port=os.environ('POSTGRES_PORT'))
+                        user=os.environ['POSTGRES_USER'],
+                        password=os.environ['POSTGRES_PASSWORD'],
+                        host=os.environ['POSTGRES_HOST'],
+                        port=os.environ['POSTGRES_PORT'])
 cursor = conn.cursor()
 
 def main():
@@ -34,10 +35,22 @@ def main():
             else:
                 print("[X] ERROR DE LOGIN")
                 envia_error(1)
+        if(login):
+            if('codigo' in usuario.keys()):
+                if(usuario['codigo']==9):
+                    print("[X] USUARIO CERRO LOGIN "+user[1])
+                    login=False 
+            else:
+                for prod in usuario['productos']:
+                    cursor.execute(f"INSERT INTO ventas (id_usuario, id_producto, cantidad, precio) VALUES ({usuario['usuario_ID']},{prod['ID']}, {prod['cantidad']}, {prod['precio']})")
+                print("[X] COMPRA REGISTRADA")
+                envia_actualizacion_bd(usuario['productos'])
+
+
     def envia_error(codigo):
         mensaje = {'codigo': codigo}
         mensaje = json.dumps(mensaje)
-        channel.basic_publish(exchange='', routing_key='user', body= mensaje)
+        channel.basic_publish(exchange='', routing_key='python-js', body= mensaje)
         print(" [x] Sent error")
         #connection.close()
 
@@ -47,14 +60,19 @@ def main():
         
         mensaje = {'codigo': 0, 'usuario': {'id': id, 'nombre': nombre}}
         mensaje = json.dumps(mensaje)
-        channel.basic_publish(exchange='', routing_key='user', body= mensaje)
+        channel.basic_publish(exchange='', routing_key='python-js', body= mensaje)
         
         #channel.basic_publish(exchange='', routing_key='query', body= "lol")
         print(" [x] Sent user ID")
         #connection.close()   
+    def envia_actualizacion_bd(productos):
+        actualizacion = {'productos': productos}
+        actualizacion = json.dumps(actualizacion)
+        channel.basic_publish(exchange='', routing_key='python-java', body= actualizacion)
+        print(" [x] Sent update to product DB")
 
 
-    channel.basic_consume(queue='user', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='js-python', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
